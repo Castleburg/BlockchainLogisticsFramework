09acf54981df0734e9672e436619ab5798d0cbee
Christian Herreborg <s160902@win.dtu.dk>
2022-02-23 18:10:18 +0100

    fOAEP max bytes

--- SawtoothClient/Tools/RsaEncryptionService.cs
+++ SawtoothClient/Tools/RsaEncryptionService.cs
@@ -11,6 +11,7 @@ namespace SawtoothClient.Tools
     public class RsaEncryptionService
     {
         private readonly RSACryptoServiceProvider _cryptoService;
+        private int MaximumAllowedBytes => ((_cryptoService.KeySize - 384) / 8) + 7; //With fOAEP
 
         public RsaEncryptionService(RSAParameters privateRsaParameters)
         {
@@ -31,7 +32,10 @@ namespace SawtoothClient.Tools
             var jsonCommand = JsonConvert.SerializeObject(sig);
             var data = Encoding.UTF8.GetBytes(jsonCommand);
 
-            //Use at keys with a length of at least 2048
+            //
+            if(data.Length > MaximumAllowedBytes)
+                throw new ArgumentException($"Too much data to sign, limit is {MaximumAllowedBytes} bytes using current key-size");
+
             var cipherText = _cryptoService.Encrypt(data, true);
 
             return new Token()
