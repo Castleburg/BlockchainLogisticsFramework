afd372236cb2b6b559f93130d5078b26ea8aba5e
Christian Herreborg <s160902@win.dtu.dk>
2022-03-17 22:26:09 +0100

    Added Demo

--- ManualTests/Program.cs
+++ ManualTests/Program.cs
@@ -32,55 +32,102 @@ namespace ManualTests
         private static void Main(string[] args)
         {
             Console.WriteLine("Starting up!");
-            var validatorAddress = "tcp://" + (args.Any() ? args.First() : "192.168.0.106:4004");
-            var clientAddress = "http://" + (args.Any() ? args.First() : "192.168.0.106:8008");
-
-            //var processor = new Processor(validatorAddress);
-            //processor.Run();
-
-            var client = new SawtoothClient.SawtoothClient(clientAddress, LogisticEnums.EntityType.RideShare, "1.0");
-
             
+            Demo();
+
+            Console.WriteLine("Done!");
+        }
+
+        public static void Demo()
+        {
+            var validatorAddress = "tcp://localhost:4004";
+            var clientAddress = "http://localhost:8008";
+
+            var processor = new Processor(validatorAddress, clientAddress);
+            processor.Run();
+
 
             var rsa = RSA.Create();
             rsa.KeySize = 2048;
             var param = rsa.ExportParameters(true);
             var publicKey = rsa.ExportRSAPrivateKey();
 
-            var command = new Command()
-            {
-                PublicKey = publicKey,
-                CommandType = LogisticEnums.Commands.NewEntity,
-                CompanyId = "HelloWorld",
-                TimeStamp = DateTime.Now,
-                TransactionId = Guid.NewGuid()
-            };
+            var companyId = "RideSharing Inc.";
 
 
+            var client = new SawtoothClient.SawtoothClient(clientAddress, LogisticEnums.EntityType.RideShare, "1.0");
             var enc = new RsaEncryptionService(param);
-            //var token = enc.AddSignature(command);
-
-            //var dec = new RsaDecryptionService();
-            //var verified = dec.VerifyToken(token);
-
-            //var lc = new LogisticsClient("HelloWorld", publicKey, client, enc);
-            //var response = lc.NewEntity(LogisticEnums.EntityType.RideShare);
+            var logicClient = new LogisticsClient(companyId, publicKey, client, enc);
+            var rideClient = new RideShareClient(logicClient);
 
 
-            var rep = GetEntityFromState(Guid.Parse("{e17d5486-e93e-46a7-bc4a-8f9cec8cec2d}"));
+            var driverId = "BobsId";
+            var passengarId = "AliceId";
 
+            var startResponse = rideClient.StartRide(driverId, "Glostrup");
+            var transactionId = startResponse.FirstOrDefault().TransactionId;
 
-            
+            var addPassengerResponse = rideClient.AddPassenger(driverId, "Glostrup", transactionId);
+            DisplayEntity(GetEntityFromState(transactionId));
 
+            var removePassengerResponse = rideClient.RemovePassenger(passengarId, "Frederiksberg", transactionId);
+            DisplayEntity(GetEntityFromState(transactionId));
 
-            //var rc = new RideShareClient(lc);
+            var endResponse = rideClient.StopRide(driverId, "Nørrebro", transactionId);
+            DisplayEntity(GetEntityFromState(transactionId));
 
-            //var batchStatus = client.GetBatchStatuses("d4f1a2f9dfed0fb2bef43cfa5812d8faec4fe8838eb3ea398505ad0e81b4f8f203e688e090de3fe9647c4124723a1abe9c0b24f5b5a6692a3da632b28c9a5063", 1);
-            //var batchContent = batchStatus.Content.ReadAsStringAsync().Result;
-            //var batchStatusResponse = JsonConvert.DeserializeObject<BatchStatusResponse>(batchContent);
+            var rsa2 = RSA.Create();
+            rsa.KeySize = 2048;
+            var param2 = rsa2.ExportParameters(true);
+            var publicKey2 = rsa2.ExportRSAPrivateKey();
 
-            Console.WriteLine("Done!");
+            var inviteResponse = rideClient.NewInvite(transactionId, publicKey2);
+            DisplayEntity(GetEntityFromState(transactionId));
+
+            var client2 = new SawtoothClient.SawtoothClient(clientAddress, LogisticEnums.EntityType.RideShare, "1.0");
+            var enc2 = new RsaEncryptionService(param2);
+            var logicClient2 = new LogisticsClient("Parking Inc.", publicKey2, client2, enc2);
+            var rideClient2 = new RideShareClient(logicClient2);
+
+            var acceptInviteResponse = rideClient2.AcceptInvite("Nørrebro Udendørs Parkering", DateTime.Now.AddDays(1), 120, transactionId);
+            DisplayEntity(GetEntityFromState(transactionId));
         }
+
+        public static void DisplayEntity(Entity entity)
+        {
+            Console.WriteLine();
+
+            Console.WriteLine($"Type: {entity.Type}");
+            Console.WriteLine($"TransactionId: {entity.TransactionId}");
+            Console.WriteLine($"CreatorId: {entity.CreatorId}");
+            Console.WriteLine($"CreatedDate: {entity.CreatedDate}");
+            Console.WriteLine($"ModifiedDate: {entity.LastModified}");
+
+            Console.WriteLine();
+            Console.WriteLine($"EventAmount: {entity.Events.Count}");
+            if (entity.SignOff.Invites.Count > 0)
+            {
+                Console.WriteLine($"EventAmount: {entity.Events.LastOrDefault().Type}");
+                Console.WriteLine($"EventAmount: {entity.Events.LastOrDefault().JsonContainer}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"IsFinal: {entity.Final}");
+            Console.WriteLine($"InviteAmount: {entity.SignOff.Invites.Count}");
+            if (entity.SignOff.Invites.Count > 0)
+                Console.WriteLine($"InviteStatus: {entity.SignOff.Invites.FirstOrDefault().InviteStatus}");
+            
+            Console.WriteLine();
+            Console.WriteLine($"SignatoryAmount: {entity.SignOff.Signatories.Count}");
+            if (entity.SignOff.Signatories.Count > 0)
+            {
+                Console.WriteLine($"SignatoryId: {entity.SignOff.Signatories.FirstOrDefault().Id}");
+                Console.WriteLine($"SignatoryContent: {entity.SignOff.Signatories.FirstOrDefault().JsonContainer}");
+            }
+            
+            Console.WriteLine();
+        }
+
 
         public static Entity GetEntityFromState(Guid transactionId)
         {
